2999a01e91d0f2b743847c560f320d31a8ec369a
joagaloppo <joaquingaloppo@gmail.com>
2023-04-08 11:08:19 -0300

    feat: Install dependencies and set up basic Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import 'dotenv/config';
+import express, { Application } from 'express';
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => res.status(404).json({ message: `Cannot ${req.method} ${req.path}` }));
+
+app.listen(port, () => {
+  console.log("\x1b[34m", "[server] listening at http://localhost:" + port, "\x1b[0m");
+});
+
+export default app;
