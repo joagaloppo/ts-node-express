0e3b26652d5271e8f26bc2b920121bd319f0a4b5
joagaloppo <joaquingaloppo@gmail.com>
2023-04-09 12:09:10 -0300

    feat: Install Prisma, set up DB connection, define schema, and create test endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 import express, { Application } from 'express';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
@@ -8,6 +12,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.json({ message: 'Hello World!' }));
+
+app.post('/user', async (req, res) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  return res.json(user);
+});
+
+app.get('/users', async (req, res) => {
+  const allItems = await prisma.user.findMany();
+  res.json(allItems);
+});
+
 app.use((req, res) => res.status(404).json({ message: `Cannot ${req.method} ${req.path}` }));
 
 export const server = app.listen(port, () => {
