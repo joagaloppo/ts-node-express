ebbe3463abf1ef73041b3fa43d10c72fd8b33b8a
joagaloppo <joaquingaloppo@gmail.com>
2023-04-11 06:23:32 -0300

    style: Improve code readability and tidiness

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import moment from 'moment';
 import passport from 'passport';
 import catchAsync from '../utils/catchAsync';
@@ -17,23 +17,19 @@ const login = catchAsync(async (req: Request, res: Response) => {
   res.json({ user, token });
 });
 
-const logout = catchAsync(async (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Logging out...' });
-});
-
 const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
 
-const googleAuthCallback = catchAsync(async (req: any, res: any, next: any) => {
+const googleAuthCallback = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   passport.authenticate('google', { session: false }, async (err, user) => {
-    if (err) {
-      return res.status(500).json({ message: 'Server error' });
-    }
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    }
-    const token = await tokenService.generateAccessToken(user, moment().add(1, 'hours'));
+    if (err) return res.status(500).json({ message: 'Server error' });
+    if (!user) return res.status(401).json({ message: 'Authentication failed' });
+    const token = await tokenService.generateAccessToken(user, moment().add(30, 'minutes'));
     return res.json({ user, token });
   })(req, res, next);
+});
+
+const logout = catchAsync(async (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'Logging out...' });
 });
 
 const authController = {
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,20 +1,18 @@
 import express from 'express';
 import passport from 'passport';
-import jwt from 'jsonwebtoken';
 import { authController, userController } from '../controllers';
 
 const router = express.Router();
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.get('/google', authController.googleAuth);
+router.get('/google/callback', authController.googleAuthCallback);
 router.post('/logout', authController.logout);
 router.get('/users', userController.getUsers);
 
 router.get('/secret', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json(req.user);
 });
-
-router.get('/google', authController.googleAuth);
-router.get('/google/callback', authController.googleAuthCallback);
 
 export default router;
