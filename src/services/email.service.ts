33e3375da6eedbc0afa4962546966fddfe66b5e6
joagaloppo <joaquingaloppo@gmail.com>
2023-04-11 10:39:15 -0300

    feat: Create email service with sendEmail and sendVerificationEmail methods

---
+++ src/services/email.service.ts
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+
+const { SMTP_HOST, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD, EMAIL_FROM } = process.env;
+if (!SMTP_HOST || !SMTP_PORT || !SMTP_USERNAME || !SMTP_PASSWORD || !EMAIL_FROM) {
+  throw new Error('Missing email configuration. Make sure you have configured the SMTP options in .env');
+}
+
+const transport = nodemailer.createTransport({
+  host: SMTP_HOST,
+  port: Number(SMTP_PORT),
+  auth: {
+    user: SMTP_USERNAME,
+    pass: SMTP_PASSWORD,
+  },
+});
+
+transport
+  .verify()
+  .then(() => console.log('[server] Email server is ready to take our messages'))
+  .catch(() => console.log('[server] Email server is not ready to take our messages. Check the configuration'));
+
+const sendEmail = async (to: string, subject: string, text: string) => {
+  const msg = { from: EMAIL_FROM, to, subject, text };
+  await transport.sendMail(msg);
+};
+
+const sendVerificationEmail = async (to: string, token: string) => {
+  const subject = 'Email Verification';
+  // replace this url with the link to the email verification page of your front-end app
+  const verificationEmailUrl = `http://localhost:4000/verify-email?token=${token}`;
+  const text = `Please verify your email by clicking on the following link: ${verificationEmailUrl}`;
+  await sendEmail(to, subject, text);
+};
+
+const emailService = {
+  transport,
+  sendEmail,
+  sendVerificationEmail,
+};
+
+export default emailService;
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import userService from './user.service';
 import authService from './auth.service';
 import tokenService from './token.service';
+import emailService from './email.service';
 
-export { userService, authService, tokenService };
+export { userService, authService, tokenService, emailService };
