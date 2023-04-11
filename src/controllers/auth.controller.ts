383ccbeab7ef854972173973241d0c69a2710b9e
joagaloppo <joaquingaloppo@gmail.com>
2023-04-11 08:05:48 -0300

    refactor: Update auth controllers with new token services for generating auth tokens

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,20 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
-import moment from 'moment';
 import passport from 'passport';
 import catchAsync from '../utils/catchAsync';
 import { authService, userService, tokenService } from '../services';
 
 const register = catchAsync(async (req: Request, res: Response) => {
   const user = await userService.createUser(req.body);
-  const token = await tokenService.generateAccessToken(user, moment().add(30, 'minutes'));
-  res.status(201).json({ user, token });
+  const tokens = await tokenService.generateAuthTokens(user.id);
+  return res.status(201).json({ user, tokens });
 });
 
 const login = catchAsync(async (req: Request, res: Response) => {
   const { username, password } = req.body;
   const user = await authService.loginWithCredentials(username, password);
-  const token = await tokenService.generateAccessToken(user, moment().add(30, 'minutes'));
-  res.json({ user, token });
+  const tokens = await tokenService.generateAuthTokens(user.id);
+  return res.status(200).json({ user, tokens });
 });
 
 const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
@@ -23,8 +22,8 @@ const googleAuthCallback = catchAsync(async (req: Request, res: Response, next: 
   passport.authenticate('google', { session: false }, async (err, user) => {
     if (err) return res.status(500).json({ message: 'Server error' });
     if (!user) return res.status(401).json({ message: 'Authentication failed' });
-    const token = await tokenService.generateAccessToken(user, moment().add(30, 'minutes'));
-    return res.json({ user, token });
+    const tokens = await tokenService.generateAuthTokens(user.id);
+    return res.status(200).json({ user, tokens });
   })(req, res, next);
 });
 
@@ -35,9 +34,9 @@ const logout = catchAsync(async (req: Request, res: Response) => {
 const authController = {
   register,
   login,
-  logout,
   googleAuth,
   googleAuthCallback,
+  logout,
 };
 
 export default authController;
