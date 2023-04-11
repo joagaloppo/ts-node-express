b275b03e7c5951f3827a846b53844c0271c2de42
joagaloppo <joaquingaloppo@gmail.com>
2023-04-11 08:02:29 -0300

    feat: Add token services

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,21 +1,70 @@
 import jwt from 'jsonwebtoken';
 import moment, { Moment } from 'moment';
-import { User } from '@prisma/client';
+import { PrismaClient, TokenTypes } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const { JWT_SECRET } = process.env;
 if (!JWT_SECRET) throw new Error('JWT secret is not defined');
 
-const generateAccessToken = (user: User, expires: Moment, secret = JWT_SECRET) => {
+const generateToken = (userId: string, expires: Moment, type: TokenTypes, secret = JWT_SECRET) => {
   const payload = {
-    sub: user.id,
+    sub: userId,
     iat: moment().unix(),
     exp: expires.unix(),
+    type,
   };
   return jwt.sign(payload, secret);
 };
 
+const saveToken = async (token: string, userId: string, expires: Moment, type: TokenTypes, blacklisted = false) => {
+  const tokenDoc = await prisma.token.create({
+    data: {
+      token,
+      type,
+      blacklisted,
+      User: { connect: { id: userId } },
+      expiresAt: expires.toDate(),
+    },
+  });
+  return tokenDoc;
+};
+
+const verifyToken = async (token: string, type: TokenTypes) => {
+  const payload = jwt.verify(token, JWT_SECRET);
+  const tokenDoc = await prisma.token.findUnique({ where: { token }, include: { User: true } });
+  if (!tokenDoc) throw new Error('Token not found');
+  if (tokenDoc.type !== type) throw new Error('Token is invalid');
+  if (tokenDoc.blacklisted) throw new Error('Token is blacklisted');
+  if (moment().isAfter(moment(tokenDoc.expiresAt))) throw new Error('Token has expired');
+  if (tokenDoc.User && tokenDoc.User.id !== payload.sub) throw new Error('Token is invalid');
+  return tokenDoc;
+};
+
+const generateAuthTokens = async (userId: string) => {
+  const accessTokenExpires = moment().add(30, 'minutes');
+  const accessToken = generateToken(userId, accessTokenExpires, TokenTypes.ACCESS);
+
+  const refreshTokenExpires = moment().add(30, 'days');
+  const refreshToken = generateToken(userId, refreshTokenExpires, TokenTypes.REFRESH);
+  await saveToken(refreshToken, userId, refreshTokenExpires, TokenTypes.REFRESH);
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExpires.toDate(),
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExpires.toDate(),
+    },
+  };
+};
+
 const tokenService = {
-  generateAccessToken,
+  generateToken,
+  saveToken,
+  verifyToken,
+  generateAuthTokens,
 };
 
 export default tokenService;
