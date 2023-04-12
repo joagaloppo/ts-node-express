0df776c4a148728006359d166961cb2e929d7348
joagaloppo <joaquingaloppo@gmail.com>
2023-04-12 10:21:11 -0300

    refactor: Integrate ApiError and error handling middlewares in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 import passport from './config/passport';
-import router from './routes/index';
+import router from './routes';
+import ApiError from './utils/ApiError';
+import { errorConverter, errorHandler } from './middlewares/error.middleware';
 
 const app: Application = express();
 
@@ -8,5 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(router);
+
+router.use((req, res, next) => next(new ApiError(404, `Cannot ${req.method} ${req.path}`)));
+app.use(errorConverter);
+app.use(errorHandler);
 
 export default app;
