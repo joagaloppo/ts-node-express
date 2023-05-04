654811e3d0865def4ba1ac54de6f8884b48d31db
joagaloppo <joaquingaloppo@gmail.com>
2023-05-04 14:29:34 -0300

    feat: Implement role based access control

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Application } from 'express';
+import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
-import cors from 'cors';
 import passport from './config/passport';
 import limiter from './middlewares/limiter.middleware';
 import morgan from './config/morgan';
@@ -10,7 +10,7 @@ import ApiError from './utils/ApiError';
 import { errorConverter, errorHandler } from './middlewares/error.middleware';
 import config from './config/config';
 
-const app: Application = express();
+const app = express();
 
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
@@ -19,20 +19,14 @@ if (config.env !== 'test') {
 }
 
 app.use(helmet());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(compression());
-
-app.use(cors());
-app.options('*', cors());
-
+app.use(cors({ origin: config.client_url }));
 app.use(passport.initialize());
-
 app.use(router);
 
-app.use((req, res, next) => next(new ApiError(404, `Cannot ${req.method} ${req.path}`)));
+app.use((req, _, next) => next(new ApiError(404, `Cannot ${req.method} ${req.path}`)));
 app.use(errorConverter);
 app.use(errorHandler);
 
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,22 +1,23 @@
 import express from 'express';
-import isAuth from '../middlewares/auth.middleware';
+import auth from '../middlewares/auth.middleware';
 import validate from '../middlewares/validate.middleware';
 import { authValidation } from '../validations';
 import { authController, userController } from '../controllers';
+import { actions } from '../config/roles';
 
 const router = express.Router();
 
 router.post('/register', validate(authValidation.register), authController.register);
-router.post('/set-password', validate(authValidation.setPassword), authController.setPassword);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/google', validate(authValidation.google), authController.google);
 router.post('/logout', validate(authValidation.logout), authController.logout);
-router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
+router.post('/set-password', validate(authValidation.setPassword), authController.setPassword);
 router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
+router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
 
 // Users management
-router.get('/users', userController.getUsers);
-router.delete('/users', userController.deleteAllUsers);
-router.get('/secret', isAuth, (req, res) => res.json(req.user));
+router.get('/users', auth(actions.users.get), userController.getUsers);
+router.delete('/users', auth(actions.users.delete), userController.deleteAllUsers);
+router.get('/secret', auth(), (req, res) => res.json(req.user));
 
 export default router;
